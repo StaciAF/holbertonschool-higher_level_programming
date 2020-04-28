214eda543362e7c0a9d743faa775b60b3a2e3410
StaciAF <aaen.it19@gmail.com>
2020-04-28 21:03:17 +0000

    adds program to pull last digit and determine is value

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = "Last digit of"
+str2 = "is"
+greaterend = "and is greater than 5"
+zero = "and is 0"
+lessend = "and is less than 6 and not 0"
+
+if number < 0:
+    lastdig = number % -10
+else:
+    lastdig = number % 10
+
+if lastdig > 5:
+    print("{} {:d} is {:d} and is greater than 5".format(str1, number, lastdig))
+
+elif lastdig == 0:
+    print("{} {:d} is {:d} and is 0".format(str1, number, lastdig))
+
+else:
+    print("{} {:d} is {:d} and {}".format(str1, number, lastdig, lessend))
