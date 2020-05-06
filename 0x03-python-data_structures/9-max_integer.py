a34c30e6d7e6340151e0d626be517bce7f8a71d4
StaciAF <aaen.it19@gmail.com>
2020-05-06 02:51:08 +0000

    added check for my_list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    newMAX = my_list[0]
-    for i in my_list:
-        if newMAX < i:
-            newMAX = i
-    return (newMAX)
+    if my_list:
+        newMAX = my_list[0]
+        for i in my_list:
+            if newMAX < i:
+                newMAX = i
+                return (newMAX)
