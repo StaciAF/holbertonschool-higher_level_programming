63ec4ade71e3cc469cbd8c2ead5453d68fd4f9b3
StaciAF <aaen.it19@gmail.com>
2020-05-06 03:33:14 +0000

    adds check if list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     newMAX = my_list[0]
     if my_list:
+        if my_list == 0:
+            return (None)
         for i in my_list:
             if newMAX < i:
                 newMAX = i
