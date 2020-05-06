4216e1ba1d5ba5a770d45f5d267d130356d3b5b9
StaciAF <aaen.it19@gmail.com>
2020-05-06 03:36:14 +0000

    adds None return for empty list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 def max_integer(my_list=[]):
     newMAX = my_list[0]
     if my_list:
-        if my_list == 0:
-            return (None)
         for i in my_list:
             if newMAX < i:
                 newMAX = i
         return (newMAX)
+    else:
+        return (None)
