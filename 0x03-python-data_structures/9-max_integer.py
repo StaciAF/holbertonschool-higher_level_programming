7f2e3913442a93181fb816fac3fda7442d810db2
StaciAF <aaen.it19@gmail.com>
2020-05-06 03:41:57 +0000

    changing logic location of empty list check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     newMAX = my_list[0]
-    if my_list:
+    if len(my_list) == 0:
+        return (None)
+    elif my_list:
         for i in my_list:
             if newMAX < i:
                 newMAX = i
         return (newMAX)
-    elif len(my_list) == 0:
-        return (None)
