83cb99fa328ea1967761ab749c20e4f249059745
StaciAF <aaen.it19@gmail.com>
2020-05-06 03:06:04 +0000

    adds function that returns a tuple with string length and first char

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def multiple_returns(sentence):
+    if (len(sentence) == 0):
+        length = 0
+        firstCHAR = None
+    else:
+        length = len(sentence)
+        firstCHAR = sentence[:1]
+    newTUPLE = (length, firstCHAR)
+    return (newTUPLE)
