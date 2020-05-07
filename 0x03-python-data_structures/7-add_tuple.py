536a44fc637ce95a8236cc176431c6f5863f01f8
StaciAF <aaen.it19@gmail.com>
2020-05-07 19:18:13 +0000

    adds function to add two tuples with parameters

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        a1 and a2 = 0
-        b1 and b2 = 0
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
-    return (res)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (tuple_c)
