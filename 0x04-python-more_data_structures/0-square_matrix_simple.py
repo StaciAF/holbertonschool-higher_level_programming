09eaa8082d85b01326b4946757401caf43b2e62b
StaciAF <aaen.it19@gmail.com>
2020-05-07 20:27:47 +0000

    corrected pep8 whitespace error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newMATRIX = [list(map(lambda j : j*j, i)) for i in matrix]
+    newMATRIX = [list(map(lambda j:j*j, i)) for i in matrix]
     return (newMATRIX)
