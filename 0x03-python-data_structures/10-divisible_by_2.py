bab8b81e92d1527cde4f0f5912a9dc195e73b977
StaciAF <aaen.it19@gmail.com>
2020-05-07 19:31:15 +0000

    changed append to call append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     newLIST = []
-    if my_list:
-        for i in range(len(my_list)):
-            if my_list[i] % 2 == 0:
-                newLIST.append(True)
-            else:
-                newLIST.append(False)
-        return(newLIST)
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            newLIST.append(True)
+        else:
+            newLIST.append(False)
+    return(newLIST)
