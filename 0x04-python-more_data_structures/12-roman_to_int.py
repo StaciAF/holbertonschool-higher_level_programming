af18a3804791c88de060bfca4fbd70f7cfa9d755
StaciAF <aaen.it19@gmail.com>
2020-05-08 04:15:41 +0000

    initial push, incomplete

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    vals = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     newVALUE = 0
     if roman_string:
         for i in range(len(roman_string)):
