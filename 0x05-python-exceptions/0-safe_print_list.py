364f1616f7c4eb744649dba2fa11df309ae1efdd
StaciAF <aaen.it19@gmail.com>
2020-05-18 17:22:53 +0000

    adds program to print elements in list and catch index error

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    z = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            z += 1
+        except IndexError:
+            break
+    print("")
+    return (z)
