d3cf67c870a97f456ed25345eff8cac268bedffd
StaciAF <aaen.it19@gmail.com>
2020-05-18 19:35:25 +0000

    added check for TypeError in except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,9 @@
 
 
 def safe_print_integer(value):
+
         try:
                 print("{:d}".format(value))
-                return (True)
-        except ValueError:
-                return (False)
+                return True
+        except (ValueError, TypeError):
+           return False
