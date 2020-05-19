917fb853a9c61358a90a239a47cede1eee9b9306
StaciAF <aaen.it19@gmail.com>
2020-05-19 20:02:16 +0000

    adds py program with Square class, initializes with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ defines an empty Square class """
+
+    def __init__(self, size):
+        """ initialize Square without value for size - size to be private
+
+        Args:
+            size: size of Square
+        """
+        self.__size = size
