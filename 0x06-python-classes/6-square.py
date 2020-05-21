974b8c92e1462b6280faa37d23670cbebf22f701
StaciAF <aaen.it19@gmail.com>
2020-05-21 16:32:11 +0000

    initial file commit, still working out the print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,7 +63,5 @@ class Square:
         if self.size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.size):
-                print("{}{}".format(" " * self.__position[1], "#" * self.size))
+            for i in range(self.__size):
+                print('#' * self.size)
