f657fa7bc67f060096024e0118921b777475ff07
StaciAF <aaen.it19@gmail.com>
2020-06-03 00:46:32 +0000

    updated def doc string with Args: and Returns:

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,13 @@ this module adds a method to check if object is instance of class or inhertance
 
 
 def inherits_from(obj, a_class):
-    """ this method checks if obj is instance of same class as inherited """
+    """ this method checks if obj is instance of same class as inherited
+    Args:
+        obj: the object passed for comparison
+        a_class: the class to be compared against
+    Returns:
+        True if object is inherited instance else, False
+    """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
