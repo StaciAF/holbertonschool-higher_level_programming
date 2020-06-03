e61b6585cf6d78b88b58c83ee99ac4f8a27fd89c
StaciAF <aaen.it19@gmail.com>
2020-06-03 00:50:01 +0000

    updated spacing in file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,7 @@ this module creates MyList class that inherits from list
 
 class MyList(list):
     """ new class inherits list """
+
     def print_sorted(self):
         """ this method prints the list with ascend sort """
         print(sorted(self))
