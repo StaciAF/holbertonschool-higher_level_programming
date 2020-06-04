8bb387528703d84db2aef720f36711c8e4773b8f
StaciAF <aaen.it19@gmail.com>
2020-06-04 19:21:17 +0000

    corrected during PLD

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -13,6 +13,22 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ this method retrieves dict represenation of Student """
-        return self.__dict__
+        new_dict = {}
+        if type(attrs) == list:
+                for value in attrs:
+                    if value in list(self.__dict__.keys()):
+                        new_dict[value] = self.__dict__[value]
+                return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ public method to replace attrs of Student """
+        if "first_name" in json:
+            self.first_name = json["first_name"]
+        if "last_name" in json:
+            self.last_name = json["last_name"]
+        if "age" in json:
+            self.age = json["age"]
