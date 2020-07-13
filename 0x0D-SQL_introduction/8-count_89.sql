f41e187101e2667db15005558f422a4312884ff7
StaciAF <aaen.it19@gmail.com>
2020-07-13 22:40:46 +0000

    adds script that displays number of records with id = 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- script displays number of records with id = 89 in first_table from database hbtn_0c_0
 -- using MySQL server, database name to be passed
-SELECT COUNT(id = 89) FROM `first_table`;
+SELECT COUNT(89) AS id FROM `first_table`;
