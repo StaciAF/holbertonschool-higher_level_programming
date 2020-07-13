826ab74bb19a38a0b9fb5f444308a13fc0b1a46f
StaciAF <aaen.it19@gmail.com>
2020-07-13 21:28:20 +0000

    corrected syntax, removed plural of DATABASES

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- scriptv creates a database hbtn_0c_0
 -- using MySQL server
-CREATE DATABASES IF NOT EXISTS `hbtn_0c_0`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- script that lists all the tables of a database
 -- using MySQL server, database will be passed
-SHOW DATABASES;
+SHOW TABLES;
