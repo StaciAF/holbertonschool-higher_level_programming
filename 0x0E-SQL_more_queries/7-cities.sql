d880d432310366bede584d92126aecbc4a048b70
StaciAF <aaen.it19@gmail.com>
2020-07-14 19:33:50 +0000

    changing create syntax, CREATE DATABASE/TABLE first then check existence

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- script creates database hbtn_0d_usa and table cities, checks for existence, with constraints
 -- using MySQL server, database name will be passed
-CREATE IF NOT EXISTS DATABASE `hbtn_0d_usa`;
-CREATE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT UNIQUE, PRIMARY KEY (id), state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT UNIQUE, PRIMARY KEY (id), state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
