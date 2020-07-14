48129fcb999dace1c714dee5d4b89a8362ab2004
StaciAF <aaen.it19@gmail.com>
2020-07-14 18:48:02 +0000

    missed grant priv in initial script

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- script creates new user with given password
 -- using MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED by 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost'
+IDENTIFIED by 'user_0d_1_pwd';
