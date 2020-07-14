4f8b990340f692aa635647b86ca754cf7aee11e0
StaciAF <aaen.it19@gmail.com>
2020-07-14 19:08:56 +0000

    missed database creation from local copy

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- script creates a table unique_id, checks for existence, id must be unique
 -- using MySQL server, database name will be passed
-CREATE TABLE IF NOT EXISTS `unique_id` (id INT(1) UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `unique_id` (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- script creates database hbtn_0d_usa and table states, checks for existence with attr constraints
 -- using MySQL server, database name will be passed
-CREATE TABLE IF NOT EXISTS `states` (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
