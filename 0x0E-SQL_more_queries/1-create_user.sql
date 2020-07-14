85a335f89179bad97a8a5eed09b9090b05868ed9
StaciAF <aaen.it19@gmail.com>
2020-07-14 18:50:55 +0000

    missed semicolon in first script

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
 -- script creates new user with given password
 -- using MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED by 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.*
-TO 'user_0d_1'@'localhost'
-IDENTIFIED by 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
