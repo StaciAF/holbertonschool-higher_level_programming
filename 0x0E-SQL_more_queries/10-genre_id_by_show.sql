e7e7367c9ca053524039bb9fde96827642b3aefa
StaciAF <aaen.it19@gmail.com>
2020-07-14 21:21:51 +0000

    adds scripts for tasks 10, 11, 12 working locally

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,3 @@
-SELECT shows.`title`, gen.`genre_id` FROM 
+-- scripts lists all shows that have at least one linked genre
+-- using MySQL server, database name will be passed
+SELECT shows.`title`, gen.`genre_id` FROM `tv_shows` AS shows INNER JOIN `tv_show_genres` AS gen ON shows.`id`= gen.`show_id` ORDER BY shows.`title`, gen.`genre_id` ASC;
---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- scripts lists all shows in hbtn_0d_tvshows
+-- using MySQL server, database name will be passed
+SELECT shows.`title`, gen.`genre_id` FROM `tv_shows` AS shows LEFT JOIN `tv_show_genres` AS gen ON shows.`id`= gen.`show_id` WHERE gen.`genre_id` IS NULL ORDER BY shows.`title`, gen.`genre_id` ASC;
