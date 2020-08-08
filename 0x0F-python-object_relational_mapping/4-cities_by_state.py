5a417f14d05afdc430301a28eab97d9ec1b484af
StaciAF <aaen.it19@gmail.com>
2020-08-08 17:43:03 +0000

    adds working scripts, PEP8 checked - tasks 0-5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+this script lists all states from db hbtn_0e_0_usa with name starting N
+takes 3 arguments mysql username, mysql password, db name
+sorted in asc order by states.id
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="127.0.0.1",
+                         port=3306, user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c_db = db.cursor()
+    sql = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN\
+    states ON cities.state_id = states.id"
+    c_db.execute(sql)
+    query_res = c_db.fetchall()
+    for row in query_res:
+        print(row)
+    c_db.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+this script lists all states from db hbtn_0e_0_usa with name starting N
+takes 3 arguments mysql username, mysql password, db name
+sorted in asc order by states.id
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="127.0.0.1",
+                         port=3306, user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c_db = db.cursor()
+    state_srch = (argv[4], )
+    c_db.execute("SELECT cities.name FROM cities\
+    INNER JOIN states on cities.state_id = states.id\
+    WHERE states.name=%s\
+    ORDER BY cities.id", state_srch)
+    query_res = c_db.fetchall()
+    complete_str = []
+    for rows in query_res:
+        city_str = str(rows).strip("()")
+        city_str = city_str.replace("'", '')
+        city_str = city_str.replace(",", "")
+        complete_str.append(city_str)
+    print(', '.join(complete_str))
+    c_db.close()
+    db.close()
