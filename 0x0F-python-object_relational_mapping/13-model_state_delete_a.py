e5c60987befa14b1922a8d227b4f8bd838b3d617
StaciAF <aaen.it19@gmail.com>
2020-08-09 19:50:15 +0000

    add script to delete all State objects with an a in name

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+this script lists all State obj from given db
+"""
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    delete_a_sql = session.query(State).filter(State.name.like('%a%')).first()
+    while (delete_a_sql is not None):
+        session.delete(delete_a_sql)
+        delete_a_sql = session.query(State).filter(
+            State.name.like('%a%')).first()
+    session.commit()
+    session.close()
