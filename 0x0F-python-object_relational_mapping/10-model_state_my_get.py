7e3f5007e82c735decd6c13a5248aa11cec6ef1d
StaciAF <aaen.it19@gmail.com>
2020-08-09 18:49:36 +0000

    adds scripts for tasks 10-11, pep8 checked

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+this script lists all State obj from given db
+"""
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st_name_arg = argv[4]
+    for state_name_sql in session.query(State).order_by(State.id).all():
+        if state_name_sql.name == st_name_arg:
+            print("{}".format(state_name_sql.id))
+            break
+    else:
+            print("Not Found")
+    session.close()
--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+this script lists all State obj from given db
+"""
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    add_new_state = State(name='Louisiana')
+    session.add(add_new_state)
+    session.commit()
+    print(add_new_state.id)
+    session.close()
