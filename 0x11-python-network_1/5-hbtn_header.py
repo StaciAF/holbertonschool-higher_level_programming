e6cb0bfb1a2f121087c4d8c5f360644665bd618c
StaciAF <aaen.it19@gmail.com>
2020-08-24 23:35:27 +0000

    adds script that displays header contents with requests package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this script fetches URL using requests package
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = argv[1]
+    req = requests.get(url)
+    print(req.headers['X-Request-Id'])
