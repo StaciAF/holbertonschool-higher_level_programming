2e85d54ca783bc1f0885cff5f227050d401dd620
StaciAF <aaen.it19@gmail.com>
2020-08-24 23:23:29 +0000

    adds script to fetch URL using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this script fetches URL using requests package
+"""
+if __name__ == '__main__':
+    import requests
+    url = 'https://intranet.hbtn.io/status'
+    req = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
