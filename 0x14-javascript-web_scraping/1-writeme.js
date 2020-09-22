4b343f2b5d012544880bfe5095faec994da85008
StaciAF <aaen.it19@gmail.com>
2020-09-22 20:56:11 +0000

    adds script to write string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+#!/usr/bin/node
+const process = require('process');
+const fileName = process.argv[2];
+const data = process.argv[3];
+const fs = require('fs');
+fs.writeFile(fileName, data, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
