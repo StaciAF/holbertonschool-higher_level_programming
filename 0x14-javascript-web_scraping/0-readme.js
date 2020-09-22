7ed5c708857e02453880579e95caaa9193afbe69
StaciAF <aaen.it19@gmail.com>
2020-09-22 19:41:44 +0000

    adds script to read file and print contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const process = require('process');
+const fileName = process.argv[2];
+const fs = require('fs');
+fs.readFile(fileName, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
