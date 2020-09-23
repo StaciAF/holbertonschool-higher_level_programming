8ed5c2c8215cc5c1a0a0b85314a49b98658ac263
StaciAF <aaen.it19@gmail.com>
2020-09-23 00:55:31 +0000

    adds script that stores get request in file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const process = require('process');
+const url = process.argv[2];
+const fileName = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fileName, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
