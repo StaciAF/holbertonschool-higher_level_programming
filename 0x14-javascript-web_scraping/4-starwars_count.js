47853a125aa6aa33586a02cc765e6e625bb8cb7d
StaciAF <aaen.it19@gmail.com>
2020-09-23 00:33:01 +0000

    adds skeleton to print details from array within json formatted body, not working locally

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const json = JSON.parse(body);
+    console.log(json);
+  }
+});
