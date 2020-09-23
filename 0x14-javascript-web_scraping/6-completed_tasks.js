02735fff84f0ae1adb3d784c3783cafeb2f2f04a
StaciAF <aaen.it19@gmail.com>
2020-09-23 04:35:26 +0000

    corrected semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-const process = require ('process');
-const request = require ('request');
+const process = require('process');
+const request = require('request');
 const url = process.argv[2];
-let count = 0;
+const count = 0;
 request(url, 'utf-8', function (err, resp, body) {
-    if (err) {
-	console.log(err);
-    } else {
-	const toDos = JSON.parse(body);
-	const userId = toDos.userId;
-	const tskDone = () => ({completed: true}) 
-	for (let i = 0; i < toDos.length; i++) {
-	    console.log(toDos[i]);
-	    }
-	console.log(userId);
-	console.log(count);
+  if (err) {
+    console.log(err);
+  } else {
+    const toDos = JSON.parse(body);
+    const userId = toDos.userId;
+    for (let i = 0; i < toDos.length; i++) {
+      console.log(toDos[i]);
     }
+    console.log(userId);
+    console.log(count);
+  }
 });
